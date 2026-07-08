63741fa2f7be1f53c412c9f9f3f951dd134cc3d2
Aditya Prasad <prasadaditya389@gmail.com>
2026-07-08 10:23:02 +0530

    added components

--- src/components/AccountCard.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const AccountCard = () => {
-  return <div>Account Card</div>;
-};
-
-export default AccountCard;
---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button  = ({text, type="button", onClick}) =>{
+  return (
+      <button type={type} onClick={onClick} className="w-full bg-blue-600 text-white py-3 rounded-lg font-semibold hover:bg-blue-700 transition duration-300">
+        {text}
+      </button>
+  );
+};
+
+export default Button;
