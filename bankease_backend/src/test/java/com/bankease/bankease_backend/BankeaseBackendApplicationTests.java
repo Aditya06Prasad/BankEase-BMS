280fac0a7067645aaff0a2af11b82f90dfab33fb
Aditya Prasad <prasadaditya389@gmail.com>
2026-07-19 23:20:54 +0530

    updates

---
+++ bankease_backend/src/test/java/com/bankease/bankease_backend/BankeaseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankease.bankease_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankeaseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
